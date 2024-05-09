9b566133b9903b281bca03fb73d2d69711778e87
Mateo Alvear <70535124+Nselb@users.noreply.github.com>
2024-05-09 16:06:21 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace FrontSOAP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // testButton
+            // 
+            this.testButton.Location = new System.Drawing.Point(391, 220);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(75, 23);
+            this.testButton.TabIndex = 0;
+            this.testButton.Text = "TestButton";
+            this.testButton.UseVisualStyleBackColor = true;
+            this.testButton.Click += new System.EventHandler(this.testButton_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(871, 540);
+            this.Controls.Add(this.testButton);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button testButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using FrontSOAP.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrontSOAP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void testButton_Click(object sender, EventArgs e)
+        {
+            Service1Client sClient = new Service1Client();
+            string response = sClient.GetData(5);
+            MessageBox.Show(response);
+            CompositeType compositeType = new CompositeType
+            {
+                BoolValue = true
+            };
+            CompositeType response2 = sClient.GetDataUsingDataContract(compositeType);
+            MessageBox.Show(response2.StringValue);
+        }
+    }
+}
